c16438b4b0b8c2185f532e06e1bb3a9c2ec56b2c
devansh-dvj <devansh.dvj@gmail.com>
2020-09-30 21:43:04 +0530

    Added routing including wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: FormComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/contact', },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
